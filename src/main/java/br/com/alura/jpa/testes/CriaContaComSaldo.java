05bb84352aa99becb82731ac092068e347640e18
felipecarvalhodesouza <desouzafelipecarvalho@gmail.com>
2021-02-24 21:53:52 -0300

    O estado Detached

--- src/main/java/br/com/alura/jpa/testes/CriaContaComSaldo.java
+++ src/main/java/br/com/alura/jpa/testes/CriaContaComSaldo.java
@@ -13,7 +13,7 @@ public class CriaContaComSaldo {
         EntityManager em = emf.createEntityManager();
         
 		Conta conta = new Conta();
-		conta.setTitular("Isabela");
+		conta.setTitular("Rogelio");
 		conta.setNumero(12345);
 		conta.setAgencia(54321);
 		conta.setSaldo(500.0);
@@ -23,7 +23,34 @@ public class CriaContaComSaldo {
 		em.persist(conta);
 		
 		em.getTransaction().commit();
-
+		em.close();
+				
+		/**
+		 *  Nenhuma atualização acontece depois que fechamos com em.close();
+		 *  Todas as contas que antes estavam em estado Managed pelo próprio o EntityManager
+		 *  passam a ser Detached quando este é fechado.
+		 */
+        EntityManager em2 = emf.createEntityManager();
+        
+        conta.setSaldo(100.0);
+        
+        em2.getTransaction().begin();
+        
+        /**
+         * Então se temos uma conta que é Detached neste momento, precisaremos alterar seu estado para o anterior.
+         * Para isso, criaremos mais um EntityManager após seu fechamento para podermos aplicar em2.merge() com a conta,
+         * transformando-a em Managed novamente.
+		 * Com isso, a verificação por cada atributo acontecerá de novo até encontrar o que foi alterado,
+		 * para então um update ser lançado no banco.
+         */
+        em2.merge(conta);
+        
+        em2.getTransaction().commit();
+        
+        /**
+         * Então deveremos ter em mente que todas as querys disparadas contra o database 
+         * são consequência da transição de estados.
+         */
 	}
 
 }
