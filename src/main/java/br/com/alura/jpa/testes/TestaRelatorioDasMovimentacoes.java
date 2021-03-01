9854fe67f139de4e6280c4dfda3039f83d5e8859
felipecarvalhodesouza <desouzafelipecarvalho@gmail.com>
2021-03-01 20:40:35 -0300

    Carregamento preguiçoso

---
+++ src/main/java/br/com/alura/jpa/testes/TestaRelatorioDasMovimentacoes.java
@@ -0,0 +1,41 @@
+package br.com.alura.jpa.testes;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+
+import br.com.alura.jpa.modelo.Conta;
+
+public class TestaRelatorioDasMovimentacoes {
+
+	public static void main(String[] args) {
+		
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("contas");
+        EntityManager em = emf.createEntityManager();
+        
+        String jpql = "select c from Conta c";
+        
+        TypedQuery<Conta> query = em.createQuery(jpql, Conta.class);
+        
+        List<Conta> contas = query.getResultList();
+        
+        for (Conta conta : contas) {
+        	System.out.println("Titular: " + conta.getTitular());
+        	System.out.println("Agência: " + conta.getAgencia());
+        	System.out.println("Número: " + conta.getNumero());
+        	/**
+        	 * Necessário imprimir a query para ver a diferença
+        	 * As movimentações só são buscadas no banco quando vão ser impressas.	
+        	 * Na JPA, todos os relacionamentos toMany são, por padrão, carregados preguiçosamente
+        	 * Carregados conforme são utilizados.
+        	 * Lazy loading
+        	 */
+        	System.out.println("Movimentações: " + conta.getMovimentacoes());			
+		}
+
+	}
+
+}
