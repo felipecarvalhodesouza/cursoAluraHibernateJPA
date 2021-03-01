edcd70086b19a0bef982f5650bf2a3d4a30e4569
felipecarvalhodesouza <desouzafelipecarvalho@gmail.com>
2021-02-28 22:06:19 -0300

    Query com ORDER BY DESC

--- src/main/java/br/com/alura/jpa/testes/TesteJPQL.java
+++ src/main/java/br/com/alura/jpa/testes/TesteJPQL.java
@@ -5,7 +5,7 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 
 import br.com.alura.jpa.modelo.Conta;
 import br.com.alura.jpa.modelo.Movimentacao;
@@ -38,15 +38,34 @@ public class TesteJPQL {
          * adicionar também um p entre : e Conta nessa mesma linha, indicando mais claramente de que se
          * trata de um parâmetro, substituindo o nome "conta" por "pConta" em setParameter().
          */
-        String jpql = "select m from Movimentacao m where m.conta = :pConta";
-
-        Query query = em.createQuery(jpql);
+        
+        /**
+         * Para fazer essa ordenação por valor em ordem decrescente, digitaremos order by 
+         * pelo m.valor como desc após :pConta na linha de sql.
+         */
+        String jpql = "select m from Movimentacao m where m.conta = :pConta order by m.valor desc";
+        
+        /**
+         * Na linha da lista, o getResultList() apresenta um warning do compilador indicando que o casting
+         * feito entre List<> e Movimentacao não é checado; afinal, só é possível saber o tipo e conteúdo
+         * da lista quando executarmos o código.
+         */
+        
+        /**
+         * Para evitar este erro, é comum colocarmos um tipo de query mais específica chamado TypedQuery<>,
+         * o qual é geralmente tipado com o que foi trazido na query, e neste caso é a Movimentacao.
+         * Portanto, todas as operações que fizermos já serão vistas como movimentações;
+         * então passaremos o class da Movimentacao em createQuery() para retirar o aviso do sistema de getResultList().
+         */
+        
+        TypedQuery<Movimentacao> query = em.createQuery(jpql, Movimentacao.class);
         query.setParameter("pConta", conta);
         List<Movimentacao> resultList = query.getResultList();
 
         for (Movimentacao movimentacao : resultList) {
             System.out.println("Descrição: " + movimentacao.getDescricao());
             System.out.println("Tipo: " + movimentacao.getTipoMovimentacao());
+            System.out.println("Valor: " + movimentacao.getValor());
         }
 	}
 
